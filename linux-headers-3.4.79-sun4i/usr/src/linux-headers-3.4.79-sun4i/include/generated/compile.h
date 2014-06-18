ba59e265e21875f740a2e9c9ecb2934297bd0edd
michalliu <michalliu@foxmail.com>
2014-06-19 05:59:54 +0800

    update headers files

--- linux-headers-3.4.79-sun4i/usr/src/linux-headers-3.4.79-sun4i/include/generated/compile.h
+++ linux-headers-3.4.79-sun4i/usr/src/linux-headers-3.4.79-sun4i/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 20 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Wed Feb 19 14:41:55 CST 2014"
+#define UTS_VERSION "#20 PREEMPT Thu Jun 19 05:32:49 CST 2014"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "ubtu"
 #define LINUX_COMPILER "gcc version 4.8.2 20130603 (prerelease) (crosstool-NG linaro-1.13.1-4.8-2013.06 - Linaro GCC 2013.06) "
